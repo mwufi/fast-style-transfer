2caf46a7b01785476ed0cfcfafe0f4d85416d188
Zen Tang <chuang.tang@yale.edu>
2018-12-02 00:31:04 +0000

    reduce content weight and increase checkpoint iterations

--- run_train.sh
+++ run_train.sh
@@ -1,19 +1,19 @@
+# Variables to set:
+# - GPUS - tells which GPU to run on
+# - IMG - title of the image
 
 # train it for this
-IMG_STYLE=examples/style/wave.jpg
-
-# training details
-CKPT_DIR=checkpoints
-GPUS=0
+STYLE_IMG=examples/style/$IMG.jpg
+CKPT_DIR=checkpoints/$IMG
 
 # path to save test images in
 TEST_DIR=..
 IMG_TEST=examples/content/chicago.jpg
 
-CUDA_VISIBLE_DEVICES=$GPUS python style.py --style $IMG_STYLE \
+CUDA_VISIBLE_DEVICES=$GPUS python style.py --style $STYLE_IMG \
 	--checkpoint-dir $CKPT_DIR \
 	--test $IMG_TEST \
 	--test-dir $TEST_DIR \
-	--content-weight 7.5e-1 \
-	--checkpoint-iterations 1000 \
+	--content-weight 4.5e0 \
+	--checkpoint-iterations 500 \
 	--batch-size 18 \
